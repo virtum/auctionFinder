b16e481ab5673b7eb74f677b1adc4c943d60078e
Virtum <pawel.filocha@gmail.com>
2017-05-21 12:13:05 +0200

    Test

--- src/test/java/com/filocha/finder/AuctionFinderImplTest.java
+++ src/test/java/com/filocha/finder/AuctionFinderImplTest.java
@@ -2,34 +2,27 @@ package com.filocha.finder;
 
 import com.filocha.messaging.client.ClientBusImpl;
 import com.filocha.messaging.server.ServerBusImpl;
-import https.webapi_allegro_pl.service.ItemsListType;
 import org.apache.log4j.BasicConfigurator;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.equalTo;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AuctionFinderImplTest {
 
-    @Autowired
+    //@Autowired
     private AuctionFinderImpl auctionFinder;
 
-    @Test
-    public void shouldFindAnyAuction() {
-        List<ItemsListType> auctions = auctionFinder.findAuctions("nokia");
-        assertThat(auctions.size(), greaterThan(0));
-    }
+//    @Test
+//    public void shouldFindAnyAuction() {
+//        List<ItemsListType> auctions = auctionFinder.findAuctions("nokia");
+//        assertThat(auctions.size(), greaterThan(0));
+//    }
 
     @Test
     public void shouldSendAndReceiveMessageViaActiveMQ() throws ExecutionException, InterruptedException {
