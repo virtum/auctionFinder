cc2e4aa9e49dc2d9540860a5ee806196ed5497bd
Filocha <pafo@gft.com>
2017-05-26 11:21:21 +0200

    Add proper implementation to test,, remove redundant code

--- src/test/java/com/filocha/security/SecurityTest.java
+++ src/test/java/com/filocha/security/SecurityTest.java
@@ -12,7 +12,6 @@ import org.springframework.security.web.FilterChainProxy;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -36,10 +35,6 @@ public class SecurityTest {
 
     private MockMvc mockMvc;
 
-    private final String SECURED_URI = "/auth";
-
-    //private final String LOGIN_PAGE_URL = "http://localhost/spring_security_login";
-
     @Before
     public void setUp() throws Exception {
         mockMvc = webAppContextSetup(wac)
@@ -48,8 +43,10 @@ public class SecurityTest {
                 .alwaysDo(print()).build();
     }
 
-    //@Test
-    public void itShouldAllowAccessToSecuredPageForPermittedUser() throws Exception {
+    @Test
+    public void shouldAllowAccessToSecuredPageForPermittedUser() throws Exception {
+        final String SECURED_URI = "/rest/account";
+
         MockHttpSession session = new MockHttpSession();
         session.setAttribute(
                 HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
@@ -71,36 +68,7 @@ public class SecurityTest {
                 SecurityContextHolder.getContext());
 
         mockMvc.perform(get(SECURED_URI).session(session))
-                .andExpect(status().isOk()).andReturn();
+                .andExpect(status().isOk());
     }
 
-    @Test
-    public void temp() throws Exception {
-        MockHttpSession session = new MockHttpSession();
-        session.setAttribute(
-                HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
-                SecurityContextHolder.getContext());
-
-        MvcResult result = mockMvc.perform(get(SECURED_URI).session(session))
-                .andExpect(status().isOk()).andReturn();
-
-        System.out.println(result.getResponse().getContentAsString());
-
-
-        UserModel user = new UserModel();
-        user.setPassword("temp");
-        user.setUserName("temp");
-
-        sessionHandler.authenticateUserAndInitializeSessionByUsername(user);
-
-        session = new MockHttpSession();
-        session.setAttribute(
-                HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
-                SecurityContextHolder.getContext());
-
-        result = mockMvc.perform(get(SECURED_URI).session(session))
-                .andExpect(status().isOk()).andReturn();
-
-        System.out.println(result.getResponse().getContentAsString());
-    }
 }
