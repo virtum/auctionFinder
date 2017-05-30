a19858ce5a9148cdaa75cac2d5f6ca4f4c33986c
Filocha <pafo@gft.com>
2017-05-30 11:30:53 +0200

    Moved code responsible for finding auctions to auctionFinderBackend

--- src/main/java/https/webapi_allegro_pl/service/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "https://webapi.allegro.pl/service.php", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package https.webapi_allegro_pl.service;
