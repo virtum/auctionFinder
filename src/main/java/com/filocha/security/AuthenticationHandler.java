0a66dc9976d2558d482c2eaf85b9376caaab6576
Virtum <pawel.filocha@gmail.com>
2018-04-08 12:00:38 +0200

    Remove redundant code during user authentication

--- src/main/java/com/filocha/security/AuthenticationHandler.java
+++ src/main/java/com/filocha/security/AuthenticationHandler.java
@@ -12,18 +12,12 @@ public class AuthenticationHandler {
      * This method is used to add new user into spring security context to authenticate him.
      *
      * @param userModel contains data about user necessary to authentication
-     * @return true if user was properly added to spring security context, otherwise false
+     * @return true if user was authenticated, otherwise false
      */
     public boolean authenticateUserAndInitializeSessionByUsername(UserAuthenticateModel userModel) {
-        try {
-            final Authentication auth = new UsernamePasswordAuthenticationToken(userModel, null, userModel.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(auth);
+        final Authentication auth = new UsernamePasswordAuthenticationToken(userModel, null, userModel.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(auth);
 
-            return true;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-
-            return false;
-        }
+        return auth.isAuthenticated();
     }
 }
--- src/main/java/com/filocha/security/UserAuthenticateModel.java
+++ src/main/java/com/filocha/security/UserAuthenticateModel.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 
 @Value
 @Builder
@@ -16,7 +17,7 @@ public class UserAuthenticateModel implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return Collections.emptyList();
     }
 
     @Override
