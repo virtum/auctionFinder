6d30b814ef6f3d4ba6fb7689d13dba209ef5d8ce
Filocha <pafo@gft.com>
2018-06-29 11:26:36 +0200

    Add new javadoc for finder package

--- src/main/java/com/filocha/finder/FindItemController.java
+++ src/main/java/com/filocha/finder/FindItemController.java
@@ -10,17 +10,24 @@ import org.springframework.web.context.request.async.DeferredResult;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
+//TODO change naming convention for this class and all related types, it creates new subscription and names are misleading
 public class FindItemController {
 
     @Autowired
     private ClientBus clientBus;
 
+    /**
+     * Sends message to external microservice in order to create new subscription for given user and item.
+     *
+     * @param request incoming request
+     * @return information that subscription was created
+     */
     @CrossOrigin
     @RequestMapping(value = "/find", method = RequestMethod.POST)
-    public DeferredResult<FindItemResponseModel> findItem(@RequestBody final FindItemRequestModel itemRequestModel) {
+    public DeferredResult<FindItemResponseModel> findItem(@RequestBody final FindItemRequestModel request) {
         final ItemFinderRequestMessage requestMessage = ItemFinderRequestMessage.builder()
-                .item(itemRequestModel.getItem())
-                .email(itemRequestModel.getEmail())
+                .item(request.getItem())
+                .email(request.getEmail())
                 .build();
 
         final CompletableFuture<ItemFinderResponseMessage> responseMessage = clientBus.sendRequest(requestMessage, ItemFinderRequestMessage.class);
