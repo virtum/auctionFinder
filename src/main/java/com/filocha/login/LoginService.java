451d841bff13824204b715ed361c69f8b5a6279f
Filocha <pafo@gft.com>
2018-07-04 11:01:25 +0200

    Remove redundant interface

--- src/main/java/com/filocha/login/LoginService.java
+++ src/main/java/com/filocha/login/LoginService.java
@@ -1,8 +1,22 @@
 package com.filocha.login;
 
-import java.security.Principal;
+import com.filocha.security.AuthenticationHandler;
+import com.filocha.security.UserAuthenticateModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
-public interface LoginService {
+import java.security.Principal;
+import java.util.Objects;
+
+@Service
+public class LoginService {
+
+    @Autowired
+    private AuthenticationHandler authenticationHandler;
+    @Autowired
+    private FacebookValidator facebookValidator;
 
     /**
      * Adds new user to spring security context, after that user will be authenticated.
@@ -11,13 +25,24 @@ public interface LoginService {
      * @param email       user email, it will be used as a user name
      * @return true if user was properly authenticated, otherwise false
      */
-    boolean authenticateUser(final String accessToken, final String email);
+    public boolean authenticateUser(final String accessToken, final String email) {
+        return !StringUtils.isEmpty(email) &&
+                authenticationHandler.authenticateUserAndInitializeSessionByUsername(UserAuthenticateModel
+                        .builder()
+                        .password(accessToken)
+                        .userName(email)
+                        .build());
+    }
 
-    /***
+    /**
      * Checks if user is authenticated or not.
      *
      * @param principal user data, contains information about user
      * @return true if user was properly authenticated, otherwise false
      */
-    boolean checkIfUserIsLogged(Principal principal);
+    public boolean checkIfUserIsLogged(Principal principal) {
+        final Authentication authentication = (Authentication) principal;
+
+        return Objects.nonNull(authentication) && authentication.isAuthenticated();
+    }
 }
